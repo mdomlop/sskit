5d9d04e3848083c789ff436a462430a3bcbff99a
Manuel <mdomlop@gmail.com>
2023-03-05 18:38:54 +0100

    Fix DESCRIPTION bug

--- src/sstools.h
+++ src/sstools.h
@@ -1,10 +1,11 @@
-#define PKGNAME      "sstools"
-#define VERSION      "0.4b"
-#define URL          "https://github.com/mdomlop/sstools"
-#define LICENSE      "GPLv3+"
-#define AUTHOR       "Manuel Domínguez López"
-#define NICK         "mdomlop"
-#define MAIL         "zqbzybc@tznvy.pbz"
+#define PKGNAME         "sstools"
+#define PKGDESCRIPTION  "KISS tools for make snapshots in a Btrfs filesystem."
+#define VERSION         "0.4b"
+#define URL             "https://github.com/mdomlop/sstools"
+#define LICENSE         "GPLv3+"
+#define AUTHOR          "Manuel Domínguez López"
+#define NICK            "mdomlop"
+#define MAIL            "zqbzybc@tznvy.pbz"
 
 void version (void)
 {
