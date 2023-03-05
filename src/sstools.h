9fab8f4bdc9cde37d62142fd9728d47a8c47bd60
Manuel <mdomlop@gmail.com>
2023-03-05 21:25:57 +0100

    Version 0.5b: Some bugs soved.

--- src/sstools.h
+++ src/sstools.h
@@ -1,6 +1,6 @@
 #define PKGNAME         "sstools"
 #define PKGDESCRIPTION  "KISS tools for make snapshots in a Btrfs filesystem."
-#define VERSION         "0.4b"
+#define VERSION         "0.5b"
 #define URL             "https://github.com/mdomlop/sstools"
 #define LICENSE         "GPLv3+"
 #define AUTHOR          "Manuel Domínguez López"
