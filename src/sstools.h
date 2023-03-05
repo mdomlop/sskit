ada416b99cffeee06031bf97afd4e5c49a386868
Manuel <mdomlop@gmail.com>
2023-03-05 10:30:51 +0100

    Add header file

---
+++ src/sstools.h
@@ -0,0 +1,12 @@
+#define PKGNAME      "sstools"
+#define VERSION      "0.3b"
+#define URL          "https://github.com/mdomlop/sstools"
+#define LICENSE      "GPLv3+"
+#define AUTHOR       "Manuel Domínguez López"
+#define NICK         "mdomlop"
+#define MAIL         "zqbzybc@tznvy.pbz"
+
+void version (void)
+{
+	printf ("%s Version: %s\n", PROGRAM, VERSION);
+}
