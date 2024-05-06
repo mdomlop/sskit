fbdd5689b8a27f76f449d3e5399f5712a7e3a843
Manuel <mdomlop@gmail.com>
2024-05-06 20:57:03 +0200

    Add OpenRC service

---
+++ dist/openrc/gentoo.mk
@@ -0,0 +1,46 @@
+############
+#   Arch   #
+############
+
+
+#ARCHI = $(shell uname -m)
+ARCHI = any
+PKGEXT=.ebuild
+GENTOOPKG = $(PKGNAME)-$(VERSION)-1-$(ARCHI)$(PKGEXT)
+
+
+$(GENTOOPKG):
+	echo '# Maintainer: $(AUTHOR) <$(MAIL)>' > $@
+	echo '_pkgver_year=2018' >> $@
+	echo '_pkgver_month=07' >> $@
+	echo '_pkgver_day=26' >> $@
+	echo 'pkgname=$(PKGNAME)' >> $@
+	echo 'pkgver=$(VERSION)' >> $@
+	echo 'pkgrel=1' >> $@
+	echo 'pkgdesc="$(DESCRIPTION)"' >> $@
+	echo 'arch=("$(ARCHI)")' >> $@
+	echo 'url="$(URL)"' >> $@
+	echo 'source=()' >> $@
+	echo 'license=("$(LICENSE)")' >> $@
+	echo 'depends=("sskit")' >> $@
+	echo 'groups=("openrc-world")' >> $@
+	echo 'package() {' >> $@
+	echo 'cd $$startdir' >> $@
+	echo 'make install DESTDIR=$$pkgdir' >> $@
+	echo '}' >> $@
+
+pkg_gentoo: $(ARCHPKG)
+$(ARCHPKG): PKGBUILD makefile sskd $(HEADERS)
+	makepkg -df PKGDEST=./ BUILDDIR=./ PKGEXT='$(PKGEXT)'
+	@echo
+	@echo Package done!
+	@echo You can install it as root with:
+	@echo pacman -U $@
+
+clean_arch:
+	rm -rf pkg
+	rm -rf src
+	rm -f PKGBUILD
+
+purge_arch: clean_arch
+	rm -f $(PKGNAME)-*$(PKGEXT)
